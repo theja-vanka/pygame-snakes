dfb4ed1ce31fedc282614a0f0fbfe464d91d0cc5
Krishnatheja Vanka <theja.vanka@gmail.com>
2021-01-15 23:52:37 +0530

    Base scaffold

--- game.py
+++ game.py
@@ -1,5 +1,21 @@
 import pygame
 import random
+from enum import Enum
+from collections import namedtuple
+
+
+pygame.init()
+
+
+class Direction(Enum):
+    RIGHT = 1
+    LEFT = 2
+    UP = 3
+    DOWN = 4
+
+
+Point = namedtuple('Point', 'x', 'y')
+BLOCK_SIZE = 20
 
 
 class SnakeGame():
@@ -8,8 +24,20 @@ class SnakeGame():
         self.h = h
 
         # init display
+        self.display = pygame.display.set_mode((self.w, self.h))
+        pygame.display.set_caption('Snakes')
+        self.clock = pygame.time.Clock()
 
         # init game style
+        self.direction = Direction.RIGHT
+        self.head = Point(self.w/2, self.h/2)
+        self.snake = [
+            self.head,
+            Point(self.head.x-BLOCK_SIZE, self.head.y),
+            Point(self.head.x-(2*BLOCK_SIZE), self.head.y)
+        ]
+        self.score = 0
+        self.food = None
 
     def play_step(self):
         pass
