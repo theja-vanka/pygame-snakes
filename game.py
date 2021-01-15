e44640eca9ff8a345a69c1f053b575153a0ad304
Krishnatheja Vanka <theja.vanka@gmail.com>
2021-01-15 23:38:47 +0530

    Initial Commit

---
+++ game.py
@@ -0,0 +1,27 @@
+import pygame
+import random
+
+
+class SnakeGame():
+    def __init__(self, w=640, h=480):
+        self.w = w
+        self.h = h
+
+        # init display
+
+        # init game style
+
+    def play_step(self):
+        pass
+
+
+if __name__ == '__main__':
+    game = SnakeGame()
+
+    # game loop
+    while True:
+        game.play_step()
+
+        # break if game over
+
+    pygame.quit()
