1ba99216b33c178757a40c38263902249b048452
Krishnatheja Vanka <theja.vanka@gmail.com>
2021-01-17 04:47:48 +0530

    Add init variables

--- agent.py
+++ agent.py
@@ -10,9 +10,14 @@ MAX_MEMORY = 100_000
 BATCH_SIZE = 1000
 LR = 0.001
 
+
 class Agent:
     def __init__(self):
-        pass
+        self.generation = 0
+        self.epsilon = 0  # randomness
+        self.gamma = 0  # discount rate
+        self.memory = deque(maxlen=MAX_MEMORY)  #popleft()
+        # TODO : model, trainer
 
     def get_state(self, game):
         pass
