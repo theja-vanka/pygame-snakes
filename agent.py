0f4af30544a255d22717009d4d66e3d0bcf6bee9
Krishnatheja Vanka <theja.vanka@gmail.com>
2021-01-17 04:56:45 +0530

    Add train function

--- agent.py
+++ agent.py
@@ -28,7 +28,7 @@ class Agent:
     def train_long_memory(self):
         pass
 
-    def train_short_memory(self):
+    def train_short_memory(self, state, action, reward, next_state, done):
         pass
 
     def get_action(self, state):
@@ -36,7 +36,43 @@ class Agent:
 
 
 def train():
-    pass
+    plot_scores = []
+    plot_mean_scores = []
+    total_score = 0
+    record = 0
+    agent = Agent()
+    game = SnakeGameAI()
+    while True:
+        # get old state
+        state_old = agent.get_state(game)
+
+        # get move
+        final_move = agent.get_action(state_old)
+
+        # perform move and get new state
+        reward, done, score = game.play_step(final_move)
+        state_new = agent.get_state(game)
+
+        # train short memory
+        agent.train_short_memory(state_old, final_move, reward, state_new, done)
+
+        # remember
+        agent.remember(state_old, final_move, reward, state_new, done)
+
+        if done:
+            # train long memory, plot result
+            game.reset()
+            agent.generation += 1
+            agent.train_long_memory()
+
+            if score > record:
+                record = score
+                # agent.model.save()
+
+            print('Generation : ', agent.generation, end=" | ")
+            print('Score : ', score, end=" | ")
+            print('Record : ', record)
+            #  TODO : Plotting
 
 
 if __name__ == '__main__':
